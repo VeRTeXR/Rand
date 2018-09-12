4a17720261ab9e31c28429872160bc0c696f13e8
VeRTeXR <p5vasut27665@gmail.com>
2018-09-12 22:54:40 +0700

    Loading Image to a new Material instance

--- Assets/Scripts/FileLoader.cs
+++ Assets/Scripts/FileLoader.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using  SimpleFileBrowser;
+using UnityEditor;
+using UnityEngine.UI;
 
-public class FileLoader : MonoBehaviour {
-
+[System.Serializable]
+public class FileLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Image _appliedImage;
+    private Texture _loadedTexture; 
+    
     public void ConfigClick()
     {
         FileBrowser.SetFilters( true, new FileBrowser.Filter( "Images", ".jpg", ".png" ), new FileBrowser.Filter( "Text Files", ".txt", ".pdf" ) );
@@ -22,5 +30,23 @@ public class FileLoader : MonoBehaviour {
         // Print whether a file is chosen (FileBrowser.Success)
         // and the path to the selected file (FileBrowser.Result) (null, if FileBrowser.Success is false)
         Debug.Log( FileBrowser.Success + " " + FileBrowser.Result );
+
+        WWW fileDir = new WWW("file://" + FileBrowser.Result);
+        while (!fileDir.isDone)
+        {
+            yield return null;
+        }
+        _loadedTexture = fileDir.texture;
+        Debug.LogError(_loadedTexture);
+        ApplyLoadedPicToImageTexture();
     }
+
+    private void ApplyLoadedPicToImageTexture()
+    {
+        Debug.LogError("1 applied "+ _appliedImage.material);
+        _appliedImage.material = new Material(Shader.Find("Sprites/Default"));
+        Debug.LogError("2 applied "+ _appliedImage);
+        _appliedImage.material.mainTexture = _loadedTexture;
+    }
+    
 }
