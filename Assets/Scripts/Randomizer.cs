291f88c1c804e6ee821b9fbf51a111643609f70b
VeRTeXR <p5vasut27665@gmail.com>
2018-09-25 00:06:32 +0700

    Repopulating shit

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -11,27 +11,55 @@ public class Randomizer : MonoBehaviour
 	void Awake ()
 	{
 		_currentEntries = null;
-			PopulateEntryList();
+		PopulateEntryList();
 	}
 
 	private void PopulateEntryList()
 	{
 		if (_currentEntries == null)
 		{
-			var loopToIndex = Manager.instance.EntryManager.ImageEntry.Count / 3;
-			_currentEntries = new List<ImageEntry>(loopToIndex);
-			for (var i = 0; i < loopToIndex; i++)
+			var loopToIndex = Manager.instance.EntryManager.ImageEntry.Count;
+			_currentEntries = new List<ImageEntry>(10);
+			for (var i = 0; i < 10; i++)
 			{
-				if(Manager.instance.EntryManager.ImageEntry[i] != null)
+				if (Manager.instance.EntryManager.ImageEntry[i] != null)
+				{
 					_currentEntries.Add(Manager.instance.EntryManager.ImageEntry[i]);
+					Debug.LogError("add : "+Manager.instance.EntryManager.ImageEntry[i]);
+					if (_currentIndex < Manager.instance.EntryManager.ImageEntry.Count)
+						_currentIndex++;
+					else
+						_currentIndex = 0;
+				}
 			}
-//		_currentIndex = _currentIndex + _currentIndex;
-//		Debug.LogError(_currentIndex);
 		}
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.A)) 
+			RepopulateEntryList();
+	}
+	
+	public void RepopulateEntryList()
+	{
+		_currentEntries = null; 
+		_currentEntries = new List<ImageEntry>(10);
+		for (var i = 0; i < _currentIndex; i++)
+		{
+			_currentEntries.Add(Manager.instance.EntryManager.ImageEntry[_currentIndex + i]);
+			Debug.LogError("add : "+Manager.instance.EntryManager.ImageEntry[_currentIndex + i]+" : "+(_currentIndex+i));
+			if (_currentIndex < Manager.instance.EntryManager.ImageEntry.Count)
+				_currentIndex++;
+			else
+				_currentIndex = 0;
+		}
+		
 	}
 
 	public List<ImageEntry> GetCurrentEntries()
 	{
+		Debug.LogError("GetCurrentEntries");
 		return _currentEntries;
 	}
 }
